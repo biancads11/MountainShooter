5f80dcb750aa98cab16651fbebff7a62c6444f57
Bianca Costa <bianca.costa@fpf.br>
2024-12-04 22:14:52 -0400

    Adjust name files Create EntityMediator.py Adjust area enemy

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from code.Const import ENTITY_SPEED, WIN_WIDTH
-from code.entity import Entity
+from code.Entity import Entity
 
 
 class Enemy(Entity):
@@ -10,5 +10,3 @@ class Enemy(Entity):
 
     def move(self, ):
         self.rect.centerx -= ENTITY_SPEED[self.name]  # velocidade
-        if self.rect.right <= 0:
-            self.rect.left = WIN_WIDTH
